17b1e79f365f2669b180baa58486611b735220ca
fvn70 <fvn_reg@mail.ru>
2021-10-30 15:11:47 +0700

    Stage 4/5: First move

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,6 @@
+import re
+
+
 def sum_chars(s, ch):
     m = [0 for c in s]
     for i in range(3):
@@ -44,4 +47,19 @@ def analyze(s):
 
 t = input("Enter cells: ")
 draw(t)
-analyze(t)
+while True:
+    digs = input("Enter the coordinates: ")
+    if not re.match("[1-9] [1-9]", digs):
+        print("You should enter numbers!")
+    elif not re.match("[1-3] [1-3]", digs):
+        print("Coordinates should be from 1 to 3!")
+    else:
+        row, col = digs.split()
+        k = 3 * (int(row) - 1) + int(col) -1
+        if t[k] == '_':
+            t = t[:k] + 'X' + t[k + 1:]
+            break
+        else:
+            print("This cell is occupied! Choose another one!")
+# analyze(t)
+draw(t)
